09d9b9f1d1d9fcbcb71d9284bb759deb56b7f76a
sidethoughts <mail@sidethoughts.org>
2026-02-20 00:03:50 +0100

    some cleanup in file

--- build.sh
+++ build.sh
@@ -14,12 +14,14 @@ if [[ -e $newFolder ]]; then
     echo "removed old"
 fi
 
+#checks all modules files for modules as well and copy them to the build directory
 check_modules() {
-    #checks all modules files for modules as well and copy them to the build directory
+    # remove old modules and copy new ones
     mkdir -p "$newModulesFolder"
     cp -r $moduleFolder $newFolder
     echo "copied modules"
 
+    # create array from all html module files and sending each file to find_in_file
     readarray -t filesArray < <(find $moduleFolder | grep "\.html*$" )
     echo "Found: "${#filesArray[@]}" html files:"
     for i in "${filesArray[@]}"; do
@@ -31,7 +33,7 @@ check_modules() {
 
 find_html_files() {
     #finding all files with .html and save them to array
-    readarray -t filesArray < <(find $1 | grep "\.html*$" )
+    readarray -t filesArray < <(find $htmlFolder | grep "\.html*$" )
     echo "Found: "${#filesArray[@]}" html files:"
     for i in "${filesArray[@]}"; do
         echo "$i"
@@ -47,22 +49,23 @@ find_html_files() {
 #$2 base dir path htmlFolder or moduleFolder
 #$3 dir where to put file newModulesFolder or newFolder
 #$4 dir where to look for modules moduleFolder oe newModulesFolder
+#looks if file contains @includes and give them to replace_html_module_file
 find_in_file() {
-    # create array from all @includes in file
+    # create array from all @includes in file 
     readarray -t includesArray < <(grep '@include' $1 )
     for i in "${includesArray[@]}"; do
         echo "$i"
     done
 
-    fileScr="${1//$2/}" #sidethoughts/index.html
-    fileName=${fileScr##*/} # index.html
-    fileDir=${fileScr%$fileName} #sidethoughts/
+    fileScr="${1//$2/}" # eg. /sidethoughts/index.html
+    fileName=${fileScr##*/} # eg. index.html
+    fileDir=${fileScr%$fileName} #eg. sidethoughts/
+    # sends every @includes to replace_html_module_file with some file checks to asure everything is right
     for include in "${includesArray[@]}"; do
         echo "$include"
         if [[ -n $include ]]; then
             echo "found @include in $1"
             moduleName="${include##*-}"
-            
             if [[ -e $moduleFolder/$moduleName ]]; then
                 echo "module exist"
                 replace_html_module_file $include "$4$moduleName" $1 $3$fileScr "$3$fileDir"
@@ -81,6 +84,7 @@ find_in_file() {
 #$3 = file to inspect e.g. ../index.html
 #$4 = file to create with module e.g. ../build/index.html
 #$5 = build dir e.g. . ../build/.../
+# replaces @includes-module.html with the module html code from its file
 replace_html_module_file() {
     #check if build dir exists if not create
     if [ ! -d "$5" ]; then
@@ -90,11 +94,11 @@ replace_html_module_file() {
         echo "Directory '$5' already exists."
     fi    
       
-    #check if file ($4) exits then read and write in this file
+    #check if file ($4) exits already then read and write in this exact file
     if [[ -e $4 ]]; then # if exist then
         echo "overwriting because file exists"
         sed -i "\,$1,r $2" "$4"
-    else # if not copy html with module in new file
+    else # if not then copy html with module in new file
     echo $3
         sed "\,$1,r $2" "$3" > "$4"
         echo "file created"
@@ -102,7 +106,7 @@ replace_html_module_file() {
 
     fi
 
-    #remove @include in new file
+    #remove @include in this file
     sed -i "\,$1,d" "$4"
 }
 
@@ -114,6 +118,6 @@ format() {
     js-beautify $homeFolder/index.html --type html --replace --indent-size 2 --max-preserve-newlines 0
 }
 
-check_modules $moduleFolder
-find_html_files $htmlFolder
+check_modules
+find_html_files
 format
